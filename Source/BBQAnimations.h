838d285318205c7f681e7a61efc267c0a456f329
Nikki Durkin <nikki@99dresses.com>
2015-02-11 19:31:35 +1100

    View is working for golden goose tiles, but without fancy animation

--- Source/BBQAnimations.h
+++ Source/BBQAnimations.h
@@ -13,12 +13,13 @@
 @interface BBQAnimations : NSObject
 
 + (void)animateButton:(CCButton *)button;
++ (void)animateScoreLabel:(CCLabelTTF *)scoreLabel;
 + (void)animateMenuWithBackground:(CCNode *)background popover:(CCNode *)popover;
 + (void)animateMarker:(CCNode *)marker;
 + (void)dismissMenuWithBackground:(CCNode *)background popover:(CCNode *)popover;
 + (void)dismissMenuWithoutTouchingBackground:(CCNode *)background popover:(CCNode *)popover;
 + (void)dismissMenu:(CCNode *)menu1 andShowMenu:(CCNode *)menu2 background:(CCNode *)background;
 + (void)animateProgressToNextLevelWithGreySteppingStones:(NSArray *)greySteppingStones yellowSteppingStones:(NSArray *)yellowSteppingStones greyLandingPad:(CCSprite *)greyLandingPad activeLandingPad:(CCNode *)activeLandingPad marker:(CCSprite *)marker;
-+ (void)animateSwipe:(NSDictionary *)animations scoreLabel:(CCLabelTTF *)scoreLabel movesLabel:(CCLabelTTF *)movesLabel cookiesLayer:(CCNode *)cookiesLayer tilesLayer:(CCNode *)tilesLayer currentScore:(NSInteger)currentScore movesLeft:(NSInteger)movesLeft gameLogic:(BBQGameLogic *)gameLogic completion:(dispatch_block_t)completion;
+
 
 @end
--- Source/GameplayScene.m
+++ Source/GameplayScene.m
@@ -165,21 +165,25 @@ static const CGFloat TileHeight = 36.0;
     return cookieNode;
 }
 
+- (void)spriteForCookie:(BBQCookie *)cookie {
+    BBQCookieNode *cookieNode = [self createCookieNodeForCookie:cookie column:cookie.column row:cookie.row];
+    cookie.sprite = cookieNode;
+    
+    //animate them
+    self.userInteractionEnabled = NO;
+    CCActionScaleTo *startSmall = [CCActionScaleTo actionWithDuration:0.05 scale:0.1];
+    CCActionScaleTo *scaleAction = [CCActionScaleTo actionWithDuration:0.4 scale:1.0];
+    CCActionCallBlock *block = [CCActionCallBlock actionWithBlock:^{
+        self.userInteractionEnabled = YES;
+    }];
+    CCActionSequence *sequence = [CCActionSequence actions:startSmall, scaleAction, block, nil];
+    [cookie.sprite runAction:sequence];
+
+}
+
 - (void)addSpritesForCookies:(NSSet *)cookies {
     for (BBQCookie *cookie in cookies) {
-        BBQCookieNode *cookieNode = [self createCookieNodeForCookie:cookie column:cookie.column row:cookie.row];
-        cookie.sprite = cookieNode;
-        
-        //animate them
-        self.userInteractionEnabled = NO;
-        CCActionScaleTo *startSmall = [CCActionScaleTo actionWithDuration:0.05 scale:0.1];
-        CCActionScaleTo *scaleAction = [CCActionScaleTo actionWithDuration:0.4 scale:1.0];
-        CCActionCallBlock *block = [CCActionCallBlock actionWithBlock:^{
-            self.userInteractionEnabled = YES;
-        }];
-        CCActionSequence *sequence = [CCActionSequence actions:startSmall, scaleAction, block, nil];
-        [cookie.sprite runAction:sequence];
-        
+        [self spriteForCookie:cookie];
     }
 }
 
@@ -191,7 +195,7 @@ static const CGFloat TileHeight = 36.0;
     NSLog(@"Swipe %@", direction);
     self.userInteractionEnabled = NO;
     NSDictionary *animations = [self.gameLogic swipe:direction];
-    [BBQAnimations animateSwipe:animations scoreLabel:_scoreLabel movesLabel:_movesLabel cookiesLayer:_cookiesLayer tilesLayer:_tilesLayer currentScore:self.gameLogic.currentScore movesLeft:self.gameLogic.movesLeft gameLogic:self.gameLogic completion:^{
+    [self animateSwipe:animations completion:^{
         self.userInteractionEnabled = YES;
         
         //check whether the player has finished the level
@@ -210,6 +214,120 @@ static const CGFloat TileHeight = 36.0;
     }];
     
 }
+
+#pragma mark - Animate Swipe
+
+- (void)animateSwipe:(NSDictionary *)animations completion:(dispatch_block_t)completion {
+    
+    const NSTimeInterval duration = 0.4;
+    
+    ////**** COMBOS ACTION BLOCK ****
+    
+    CCActionCallBlock *performCombosAndMoveCookies = [CCActionCallBlock actionWithBlock:^{
+        
+        ////COMBOS
+        for (BBQComboAnimation *combo in animations[COMBOS]) {
+            
+            //Put cookie A on top and move cookie A to cookie B, then remove cookie A
+            combo.cookieA.sprite.zOrder = 100;
+            combo.cookieB.sprite.zOrder = 90;
+            
+            CCActionMoveTo *moveA = [CCActionMoveTo actionWithDuration:duration position:[GameplayScene pointForColumn:combo.destinationColumn row:combo.destinationRow]];
+            CCActionRemove *removeA = [CCActionRemove action];
+            
+            CCActionCallBlock *updateCountCircle = [CCActionCallBlock actionWithBlock:^{
+                
+                if (combo.cookieB.isFinalCookie) {
+                    combo.cookieB.sprite.countCircle.visible = NO;
+                    combo.cookieB.sprite.tickSprite.visible = YES;
+                }
+                
+                else {
+                    //combo.cookieB.sprite.countCircle.visible = YES;
+                    combo.cookieB.sprite.countLabel.string = [NSString stringWithFormat:@"%ld", (long)combo.cookieB.count];
+                }
+                
+                //scale up and down
+                CCActionScaleTo *scaleUp = [CCActionScaleTo actionWithDuration:0.1 scale:1.2];
+                CCActionScaleTo *scaleDown = [CCActionScaleTo actionWithDuration:0.1 scale:1.0];
+                CCActionSequence *scaleSequence = [CCActionSequence actions:scaleUp, scaleDown, nil];
+                [combo.cookieB.sprite runAction:scaleSequence];
+                
+                //Display score label
+                if (combo.score > 0) {
+                    NSString *scoreString = [NSString stringWithFormat:@"%ld", (long)combo.score];
+                    CCLabelTTF *scoreLabel = [CCLabelTTF labelWithString:scoreString fontName:@"GillSans-BoldItalic" fontSize:12.0];
+                    scoreLabel.position = [GameplayScene pointForColumn:combo.destinationColumn row:combo.destinationRow];
+                    scoreLabel.outlineColor = [CCColor blackColor];
+                    scoreLabel.outlineWidth = 1.0;
+                    scoreLabel.zOrder = 300;
+                    [_cookiesLayer addChild:scoreLabel];
+                    [BBQAnimations animateScoreLabel:scoreLabel];
+                }
+                
+                //If it is a static tile, break the tile
+                if (combo.didBreakOutOfStaticTile == YES) {
+                    BBQTile *tileB = [self.gameLogic.level tileAtColumn:combo.cookieB.column row:combo.cookieB.row];
+                    
+                    if (tileB.overlayTile) {
+                        if (tileB.staticTileCountdown > 0) {
+                            tileB.overlayTile.countLabel.string = [NSString stringWithFormat:@"%ld", (long)tileB.staticTileCountdown];
+                        }
+                        
+                        else if (tileB.staticTileCountdown <= 0) {
+                            [tileB.overlayTile removeFromParent];
+                            tileB.overlayTile = nil;
+                        }
+                    }
+                    
+                    else if (tileB.tileType == 1) {
+                        [tileB.sprite removeFromParent];
+                        CCNode *newSprite = [CCBReader load:@"Tiles/RegularTile"];
+                        newSprite.position = [GameplayScene pointForColumn:combo.cookieB.column row:combo.cookieB.row];
+                        [_tilesLayer addChild:newSprite];
+                        tileB.sprite = newSprite;
+                    }
+                }
+                
+            }];
+            
+            CCActionSequence *sequenceA = [CCActionSequence actions:moveA, removeA, updateCountCircle, nil];
+            [combo.cookieA.sprite runAction:sequenceA];
+            
+        }
+        
+        ////MOVE COOKIES
+        for (BBQMoveCookie *movement in animations[MOVEMENTS]) {
+            CGPoint position = [GameplayScene pointForColumn:movement.destinationColumn row:movement.destinationRow];
+            CCActionMoveTo *moveAnimation = [CCActionMoveTo actionWithDuration:duration position:position];
+            [movement.cookieA.sprite runAction:moveAnimation];
+        }
+        
+    }];
+    
+    
+    //**** UPDATE SCORE & MOVES ****
+    CCActionCallBlock *updateScoreBlock = [CCActionCallBlock actionWithBlock:^{
+        NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+        [formatter setNumberStyle:NSNumberFormatterDecimalStyle];
+        _scoreLabel.string = [formatter stringFromNumber:@(self.gameLogic.currentScore)];
+        _movesLabel.string = [NSString stringWithFormat:@"%ld", (long)self.gameLogic.movesLeft];
+        NSLog(@"Moves left label: %@", _movesLabel.string);
+    }];
+    
+    //**** CREATE SPRITES FOR NEW GOOSE EGG COOKIES ****
+    CCActionCallBlock *newCookieSprites = [CCActionCallBlock actionWithBlock:^{
+        NSArray *newCookies = animations[GOLDEN_GOOSE_COOKIES];
+        for (BBQCookie *cookie in newCookies) {
+            [self spriteForCookie:cookie];
+        }
+    }];
+    
+    ////**** FINAL SEQUENCE ****
+    CCActionSequence *finalSequence = [CCActionSequence actions:performCombosAndMoveCookies, updateScoreBlock, newCookieSprites, [CCActionCallBlock actionWithBlock:completion], nil];
+    [_cookiesLayer runAction:finalSequence];
+}
+
 
 
 #pragma mark - Gesture Recognizers
